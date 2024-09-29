6c4598ba8d6c5a5ddd8dc73d84b30ab6996b6d03
RasmusBlegvad <you@example.comrschal21student.aau.dk>
2024-09-29 22:55:20 +0200

    addede dynamically changing ui and it works now maybe look ad the padding variables at some point

--- src/App.cpp
+++ src/App.cpp
@@ -15,18 +15,18 @@ App::App(const Screen& screen, const char* title, Color bgColor, Color border_co
      m_default_border_color(border_color),
      m_fs_rect({
         .x = m_screen.w_padding, .y = m_screen.h_padding, .width = m_screen.width / 6,
-        .height = m_screen.height / 2 + (2 * screen.w_padding)
+        .height = m_screen.height / 2 + (2 * m_screen.w_padding)
      }),
      m_comp_sig_rect(
         {
-           .x = m_fs_rect.x + m_fs_rect.width + m_screen.w_padding, .y = m_screen.h_padding,
-           .width = (m_screen.width - m_screen.w_padding * 2) - (m_fs_rect.width + m_screen.w_padding),
-           .height = m_fs_rect.height
+           .x = m_screen.w_padding + m_screen.width / 6 + m_screen.h_padding, .y = m_screen.h_padding,
+           .width = (m_screen.width - m_screen.w_padding * 2) - (m_screen.width / 6 + m_screen.h_padding),
+           .height = m_screen.height / 2 + (2 * m_screen.w_padding)
         }),
      m_part_sig_rect({
-        .x = m_screen.w_padding, .y = m_fs_rect.height + m_screen.h_padding * 2,
+        .x = m_screen.w_padding, .y = m_screen.height / 2 + (2 * m_screen.w_padding) + m_screen.h_padding * 2,
         .width = m_screen.width - m_screen.w_padding * 2.0f,
-        .height = m_screen.height - (3 * m_screen.h_padding + m_fs_rect.height)
+        .height = m_screen.height - (3 * m_screen.h_padding + m_screen.height / 2 + (2 * m_screen.w_padding))
      })
 
 {
@@ -55,24 +55,14 @@ void App::game_loop()
 
 void App::render_borders()
 {
-   //TODO: REFACTOR X AND Y VALUES INTO MEMBER VARIABLES (a lot faster maybe ???)
    //files overview rect
    DrawRectangleLinesEx(m_fs_rect, 3, m_default_border_color);
-   // DrawText("files", m_fs_rect.width / 2 + m_fs_rect.x, m_fs_rect.height / 2 + m_fs_rect.y,
-   // 20, WHITE);
 
    // composite signal rect
-
    DrawRectangleLinesEx(m_comp_sig_rect, 3, m_default_border_color);
-   // DrawText("comp signal", m_comp_sig_rect.width / 2 + m_comp_sig_rect.x,
-   // m_comp_sig_rect.height / 2 + m_screen.h_padding,
-   // 20, WHITE);
 
    // partial signals rectø
    DrawRectangleLinesEx(m_part_sig_rect, 3, m_default_border_color);
-   // DrawText("part signal", m_part_sig_rect.width / 2 + m_part_sig_rect.x,
-   // m_part_sig_rect.height / 2 + m_part_sig_rect.y,
-   // 20, WHITE);
 
 
    // options devider
@@ -115,21 +105,14 @@ void App::update_screen_size()
 {
    if (IsWindowResized())
    {
-      std::cout << "Before modifying screen: " << std::endl;
-      std::cout << "Height: " << m_screen.height << ", Width: " << m_screen.width << std::endl;
-      std::cout << "Height Padding: " << m_screen.h_padding << ", Width Padding: " << m_screen.w_padding << std::endl;
-
       // NEW SCREEN
       m_screen.height = GetScreenHeight();
       m_screen.width = GetScreenWidth();
       m_screen.h_padding = GetScreenHeight() * 0.01f;
       m_screen.w_padding = GetScreenWidth() * 0.01f;
 
-      std::cout << "After modifying screen: " << std::endl;
-      std::cout << "Height: " << m_screen.height << ", Width: " << m_screen.width << std::endl;
-      std::cout << "Height Padding: " << m_screen.h_padding << ", Width Padding: " << m_screen.w_padding << std::endl;
-
       // NEW UI RECTANGLES
+      // NOTE: h_padding is used for spacing rectangles both vertically and horisontally maybe fix later
       Rectangle new_fs_rect = {
          .x = m_screen.w_padding, .y = m_screen.h_padding, .width = m_screen.width / 6,
          .height = m_screen.height / 2 + (2 * m_screen.w_padding)
@@ -137,26 +120,17 @@ void App::update_screen_size()
 
       Rectangle new_comp_sig_rect =
       {
-         .x = m_fs_rect.x + m_fs_rect.width + m_screen.w_padding, .y = m_screen.h_padding,
-         .width = (m_screen.width - m_screen.w_padding * 2) - (m_fs_rect.width + m_screen.w_padding),
-         .height = m_fs_rect.height
+         .x = m_screen.w_padding + m_screen.width / 6 + m_screen.h_padding, .y = m_screen.h_padding,
+         .width = (m_screen.width - m_screen.w_padding * 2) - (m_screen.width / 6 + m_screen.h_padding),
+         .height = m_screen.height / 2 + (2 * m_screen.w_padding)
       };
       Rectangle new_part_sig_rect = {
-         .x = m_screen.w_padding, .y = m_fs_rect.height + m_screen.h_padding * 2,
+         .x = m_screen.w_padding, .y = m_screen.height / 2 + (2 * m_screen.w_padding) + m_screen.h_padding * 2,
          .width = m_screen.width - m_screen.w_padding * 2.0f,
-         .height = m_screen.height - (3 * m_screen.h_padding + m_fs_rect.height)
+         .height = m_screen.height - (3 * m_screen.h_padding + m_screen.height / 2 + (2 * m_screen.w_padding))
       };
 
-      std::cout << "Before updating m_fs_rect: " << std::endl;
-      std::cout << "m_fs_rect - X: " << m_fs_rect.x << ", Y: " << m_fs_rect.y
-         << ", Width: " << m_fs_rect.width << ", Height: " << m_fs_rect.height << std::endl;
-
       m_fs_rect = new_fs_rect;
-
-      std::cout << "After updating m_fs_rect: " << std::endl;
-      std::cout << "m_fs_rect - X: " << m_fs_rect.x << ", Y: " << m_fs_rect.y
-         << ", Width: " << m_fs_rect.width << ", Height: " << m_fs_rect.height << std::endl;
-
       m_comp_sig_rect = new_comp_sig_rect;
       m_part_sig_rect = new_part_sig_rect;
    }
